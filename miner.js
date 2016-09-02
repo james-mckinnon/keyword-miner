27da72a91c1797a50c6b70c1969aa037925ea8f9
James McKinnon <james.mckinnon@wildfireinternet.co.uk>
2016-09-02 15:58:42 +0100

    Accepting encryption was causing errors

--- miner.js
+++ miner.js
@@ -44,7 +44,6 @@
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, sdch',
         'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive'
